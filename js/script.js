5a6af1bef05513632ba6d8e3e357c7a16c40cb41
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-09-23 21:42:24 +0600

    modal issue fixed

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ function donateNow(inputId, donationCountId, balanceId){
     const balance = parseFloat(document.getElementById(balanceId).innerText);
     if (isNaN(inputValue) || inputValue < 0 || inputValue > balance) {
         alert('Invalid Amount');
-        window.location.reload();
+        const modal = document.getElementById('my_modal_1');
+        modal.close();
         return;
     }else{
 
